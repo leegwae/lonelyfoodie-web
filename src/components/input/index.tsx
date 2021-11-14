7c5eb26179d59bef4f8418e8b195594ec82c4bc1
leegwae <leegwae@gmail.com>
2021-11-14 20:29:28 +0900

    Add input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,34 @@
+import React, {
+	useState,
+	useImperativeHandle,
+	forwardRef,
+	InputHTMLAttributes,
+} from 'react';
+import styled from 'styled-components';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<string, InputProps>((props, ref) => {
+	const [value, setValue] = useState<string>('');
+	useImperativeHandle(ref, () => value, [value]);
+
+	return (
+		<StyledInput
+			{...props}
+			type="text"
+			size={40}
+			value={value}
+			onChange={(e) => setValue(e.target.value)}
+		/>
+	);
+});
+Input.displayName = 'Input';
+
+const StyledInput = styled('input')({
+	padding: '0px 10px',
+	lineHeight: '40px',
+	fontSize: '20px',
+	borderRadius: '4px',
+});
+
+export default Input;
