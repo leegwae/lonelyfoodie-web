bd8926f4089865128869216b4c795166556b3041
leegwae <leegwae@gmail.com>
2021-11-16 00:00:47 +0900

    Add example

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from '@components/button';
 
 const Login = () => {
-	return <div>LOGIN</div>;
+	const handleClick = () => console.log('HELLO WORLD!');
+
+	return <Button onClick={handleClick}>나를 클릭하세요</Button>;
 };
 
 export default Login;
