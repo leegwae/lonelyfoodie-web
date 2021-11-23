48912c673dca0a3a5f93f68fb7b200e7c79245ad
leegwae <leegwae@gmail.com>
2021-11-23 17:58:14 +0900

    Add template for pages associated user account

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,73 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
+import Template from '@components/template';
 
 const Login = () => {
-	const handleClick = () => console.log('HELLO WORLD!');
-
 	return (
 		<>
-			<StyledWrapper>
-				<StyledInnerWrapper>
-					<TitleWrapper>
-						<StyledIcon />
-						<Title>고독한 시식가 로그인하기</Title>
-					</TitleWrapper>
-					<Typography>서울시립대 근처 맛집을 찾아보세요</Typography>
-					<ButtonWrapper>
-						<StyledButton>카카오로 로그인하기</StyledButton>
-						<LinkWrapper>
-							계정이 없다면&nbsp;
-							<StyledLink to="/signup">회원가입하세요</StyledLink>
-						</LinkWrapper>
-					</ButtonWrapper>
-				</StyledInnerWrapper>
-			</StyledWrapper>
+			<Template
+				title="고독한 시식가 로그인"
+				sub="서울시립대 근처 맛집을 찾아보세요"
+			>
+				<StyledWrapper>
+					<StyledButton>카카오로 로그인하기</StyledButton>
+					<LinkWrapper>
+						계정이 없다면&nbsp;
+						<StyledLink to="/signup">회원가입하세요</StyledLink>
+					</LinkWrapper>
+				</StyledWrapper>
+			</Template>
 		</>
 	);
 };
 
 const StyledWrapper = styled('div')({
 	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-	flex: 1,
-});
-
-const StyledInnerWrapper = styled('div')({
-	display: 'flex',
-	flexDirection: 'column',
-	width: '700px',
-});
-
-const TitleWrapper = styled('div')({
-	display: 'flex',
-	alignItems: 'center',
-});
-
-const StyledIcon = styled(RestaurantIcon)({
-	color: '#F55919',
-	fontSize: 50,
-	marginRight: 10,
-});
-
-const Title = styled('h1')({
-	fontSize: '40px',
-	fontWeight: 'bold',
-	margin: 0,
-});
-
-const Typography = styled('h2')({
-	fontWeight: 'lighter',
-	marginTop: 5,
-});
-
-const ButtonWrapper = styled('div')({
-	display: 'flex',
 	flexDirection: 'column',
 	alignItems: 'center',
-	padding: '70px 0px',
 });
 
 const StyledButton = styled('button')({
--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
+import Template from '@components/template';
 
 const Signup = () => {
 	return (
 		<>
-			<StyledWrapper>
+			<Template
+				title="고독한 시식가 회원가입"
+				sub="고독한 시식가에 참여하세요"
+			>
+				<StyledWrapper>
+					<StyledButton>카카오로 회원가입하기</StyledButton>
+					<LinkWrapper>
+						계정이 있다면&nbsp;
+						<StyledLink to="/login">로그인하세요</StyledLink>
+					</LinkWrapper>
+				</StyledWrapper>
+			</Template>
+			{/* <StyledWrapper>
 				<StyledInnerWrapper>
 					<TitleWrapper>
 						<StyledIcon />
@@ -21,51 +33,15 @@ const Signup = () => {
 						</LinkWrapper>
 					</ButtonWrapper>
 				</StyledInnerWrapper>
-			</StyledWrapper>
+			</StyledWrapper> */}
 		</>
 	);
 };
 
 const StyledWrapper = styled('div')({
 	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-	flex: 1,
-});
-
-const StyledInnerWrapper = styled('div')({
-	display: 'flex',
-	flexDirection: 'column',
-	width: '700px',
-});
-
-const TitleWrapper = styled('div')({
-	display: 'flex',
-	alignItems: 'center',
-});
-
-const StyledIcon = styled(RestaurantIcon)({
-	color: '#F55919',
-	fontSize: 50,
-	marginRight: 10,
-});
-
-const Title = styled('h1')({
-	fontSize: '40px',
-	fontWeight: 'bold',
-	margin: 0,
-});
-
-const Typography = styled('h2')({
-	fontWeight: 'lighter',
-	marginTop: 5,
-});
-
-const ButtonWrapper = styled('div')({
-	display: 'flex',
 	flexDirection: 'column',
 	alignItems: 'center',
-	padding: '70px 0px',
 });
 
 const StyledButton = styled('button')({
